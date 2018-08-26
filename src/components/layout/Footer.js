f5ad2ad36016727c5f0078bf580a63c1239c8977
julio <empjulio@hotmail.com>
2018-08-25 21:33:26 -0400

    select onChange work

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -8,9 +8,13 @@ export default ({ muscles, category, onSelect }) => {
   const index = category 
       ? muscles.findIndex(group => group === category) + 1 : 0;
 
+const onIndexSelect = (e, index) => 
+        onSelect( index === 0 ? '' : muscles[index - 1] )
+
   return <Paper>
     <Tabs
       value={ index }
+      onChange={onIndexSelect}
       indicatorColor="primary"
       textColor="primary"
       centered
